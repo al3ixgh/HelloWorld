a2758a559bb25ee09c45d8a9e87ac26acfb786ac
aleix <al3ixgh@gmail.com>
2020-11-30 13:23:19 +0100

    segundo commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,80 @@
 'use strict';
 
-console.log('hola react');
+var personaA = {
+    nombre: 'Perico',
+    edad: 26,
+    city: 'Reus'
+};
+
+var personaB = {
+    nombre: 'Sultana',
+    edad: 26,
+    city: 'Riba Roja d\'Ebre'
+};
+
+console.log(personaA.nombre);
+
+var sumar = function sumar(e) {
+    personaA.edad = personaA.edad + 1;
+    //personaA.edad += 1
+    //personaA.edad ++
+    render();
+};
+
+var restar = function restar(e) {
+    personaA.edad = personaA.edad - 1;
+    //personaA.edad -= 1
+    //personaA.edad --
+    render();
+};
+
+var reset = function reset(e) {
+    personaA.edad = 0;
+    //personaA.edad += 1
+    //personaA.edad ++
+    render();
+};
+
+var render = function render() {
+
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            personaA.nombre
+        ),
+        React.createElement(
+            'p',
+            null,
+            personaA.edad
+        ),
+        React.createElement(
+            'p',
+            null,
+            personaA.city
+        ),
+        React.createElement(
+            'button',
+            { onClick: sumar },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: restar },
+            '-1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'reset'
+        )
+    );
+
+    //const appRoot = document.getElementById('appRoot')
+
+    ReactDOM.render(template, appRoot);
+};
+
+render();
--- src/app.js
+++ src/app.js
@@ -1,1 +1,57 @@
-console.log('hola react');
+const personaA = {
+    nombre: 'Perico',
+    edad: 26,
+    city: 'Reus'
+}
+
+const personaB = {
+    nombre: 'Sultana',
+    edad: 26,
+    city: 'Riba Roja d\'Ebre'
+}
+
+console.log(personaA.nombre);
+
+const sumar = (e)=>{
+    personaA.edad=personaA.edad+1
+    //personaA.edad += 1
+    //personaA.edad ++
+    render()
+}
+
+const restar = (e)=>{
+    personaA.edad=personaA.edad-1
+    //personaA.edad -= 1
+    //personaA.edad --
+    render()
+}
+
+
+const reset = (e)=>{
+    personaA.edad=0
+    //personaA.edad += 1
+    //personaA.edad ++
+    render()
+}
+
+
+const render = ()=>{
+
+    const template = (
+    <div>
+        <h1>{personaA.nombre}</h1>
+        <p>{personaA.edad}</p>
+        <p>{personaA.city}</p>
+        <button onClick= {sumar}>+1</button>
+        <button onClick={restar}>-1</button>
+        <button onClick={reset}>reset</button>
+    </div>
+    )
+
+    //const appRoot = document.getElementById('appRoot')
+
+    ReactDOM.render(template, appRoot)
+
+}
+
+render()
