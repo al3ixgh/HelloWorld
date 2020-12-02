2e2126485688177637d19c820269b0333eda2265
aleix <al3ixgh@gmail.com>
2020-12-02 09:49:39 +0100

    app-countries acabado

--- public/js/index.js
+++ public/js/index.js
@@ -2,110 +2,128 @@
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-var personaA = {
-    nombre: 'Perico',
-    edad: 26,
-    city: 'Reus'
-};
+var getCountries = function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+        var result;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+            while (1) {
+                switch (_context.prev = _context.next) {
+                    case 0:
+                        _context.next = 2;
+                        return axios.get('https://restcountries.eu/rest/v2/all');
 
-var personaB = {
-    nombre: 'Sultana',
-    edad: 26,
-    city: 'Riba Roja d\'Ebre'
-};
+                    case 2:
+                        result = _context.sent;
 
-console.log(personaA.nombre);
+                        console.log('result: ', result.data);
+                        return _context.abrupt('return', result.data);
 
-var sumar = function sumar(e) {
-    personaA.edad = personaA.edad + 1;
-    //personaA.edad += 1
-    //personaA.edad ++
-    render();
-};
+                    case 5:
+                    case 'end':
+                        return _context.stop();
+                }
+            }
+        }, _callee, undefined);
+    }));
 
-var restar = function restar(e) {
-    personaA.edad = personaA.edad - 1;
-    //personaA.edad -= 1
-    //personaA.edad --
-    render();
-};
+    return function getCountries() {
+        return _ref.apply(this, arguments);
+    };
+}();
 
-var reset = function reset(e) {
-    personaA.edad = 0;
-    render();
-};
+var countries = getCountries();
 
-var render = function render() {
+var getCountry = function () {
+    var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(countryCode) {
+        var result, countries, country;
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+            while (1) {
+                switch (_context2.prev = _context2.next) {
+                    case 0:
+                        _context2.next = 2;
+                        return axios.get('https://restcountries.eu/rest/v2/all');
 
+                    case 2:
+                        result = _context2.sent;
+                        countries = result.data;
+                        country = countries.find(function (country) {
+                            return country.alpha2Code.toLowerCase() === countryCode.toLowerCase();
+                        });
+
+                        console.log(country.name);
+                        return _context2.abrupt('return', country);
+
+                    case 7:
+                    case 'end':
+                        return _context2.stop();
+                }
+            }
+        }, _callee2, undefined);
+    }));
+
+    return function getCountry(_x) {
+        return _ref2.apply(this, arguments);
+    };
+}();
+
+var country = getCountry('en');
+
+var getCountryNameByIP = function () {
+    var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3() {
+        var ipInfo, countryCode, result, countries, country;
+        return regeneratorRuntime.wrap(function _callee3$(_context3) {
+            while (1) {
+                switch (_context3.prev = _context3.next) {
+                    case 0:
+                        _context3.next = 2;
+                        return axios.get('https://ipinfo.io/json?token=2ad24e9889b095');
+
+                    case 2:
+                        ipInfo = _context3.sent;
+
+                        console.log('ipInfo: ', ipInfo.data.country);
+                        countryCode = ipInfo.data.country;
+                        _context3.next = 7;
+                        return axios.get('https://restcountries.eu/rest/v2/all');
+
+                    case 7:
+                        result = _context3.sent;
+                        countries = result.data;
+                        country = countries.find(function (country) {
+                            return country.alpha2Code.toLowerCase() === countryCode.toLowerCase();
+                        });
+
+                        console.log(country.name);
+                        render(country.name);
+
+                    case 12:
+                    case 'end':
+                        return _context3.stop();
+                }
+            }
+        }, _callee3, undefined);
+    }));
+
+    return function getCountryNameByIP() {
+        return _ref3.apply(this, arguments);
+    };
+}();
+
+getCountryNameByIP();
+
+var appRoot = document.getElementById('appRoot');
+
+var render = function render(country) {
     var template = React.createElement(
         'div',
         null,
         React.createElement(
-            'h1',
-            null,
-            personaA.nombre
-        ),
-        React.createElement(
             'p',
             null,
-            personaA.edad
-        ),
-        React.createElement(
-            'p',
-            null,
-            personaA.city
-        ),
-        React.createElement(
-            'button',
-            { onClick: sumar },
-            '+1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: restar },
-            '-1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: reset },
-            'reset'
+            'Te encuentras en: ',
+            country
         )
     );
-
-    var key = '6041ed75bb19debef4838826c9c456a5';
-    var city = 'Reus';
-
-    var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + key;
-    var getWeather = function () {
-        var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-            var result;
-            return regeneratorRuntime.wrap(function _callee$(_context) {
-                while (1) {
-                    switch (_context.prev = _context.next) {
-                        case 0:
-                            _context.next = 2;
-                            return axios.get(url);
-
-                        case 2:
-                            result = _context.sent;
-
-                            console.log(result.data.main);
-
-                        case 4:
-                        case 'end':
-                            return _context.stop();
-                    }
-                }
-            }, _callee, undefined);
-        }));
-
-        return function getWeather() {
-            return _ref.apply(this, arguments);
-        };
-    }();
-
-    getWeather();
-    //const appRoot = document.getElementById('appRoot')
 
     ReactDOM.render(template, appRoot);
 };
--- src/app-person.js
+++ src/app-person.js
@@ -1,53 +1,53 @@
-const personaA = {
-    nombre: 'Perico',
-    edad: 26,
-    city: 'Reus'
-}
+ const personaA = {
+     nombre: 'Perico',
+     edad: 26,
+     city: 'Reus'
+ }
 
-const personaB = {
-    nombre: 'Sultana',
-    edad: 26,
-    city: 'Riba Roja d\'Ebre'
-}
+ const personaB = {
+     nombre: 'Sultana',
+     edad: 26,
+     city: 'Riba Roja d\'Ebre'
+ }
 
-console.log(personaA.nombre);
+ console.log(personaA.nombre);
 
-const sumar = (e)=>{
-    personaA.edad=personaA.edad+1
-    //personaA.edad += 1
-    //personaA.edad ++
-    render()
-}
+ const sumar = (e)=>{
+     personaA.edad=personaA.edad+1
+     //personaA.edad += 1
+     //personaA.edad ++
+     render()
+ }
 
-const restar = (e)=>{
-    personaA.edad=personaA.edad-1
-    //personaA.edad -= 1
-    //personaA.edad --
-    render()
-}
-
-
-const reset = (e)=>{
-    personaA.edad=0
-    render()
-}
+ const restar = (e)=>{
+     personaA.edad=personaA.edad-1
+     //personaA.edad -= 1
+     //personaA.edad --
+     render()
+ }
 
 
-const render = ()=>{
+ const reset = (e)=>{
+     personaA.edad=0
+     render()
+ }
 
-    const template = (
-    <div>
-        <h1>{personaA.nombre}</h1>
-        <p>{personaA.edad}</p>
-        <p>{personaA.city}</p>
+
+ const render = ()=>{
+
+     const template = (
+     <div>
+         <h1>{personaA.nombre}</h1>
+         <p>{personaA.edad}</p>
+         <p>{personaA.city}</p>
 
         
-        <button onClick={sumar}>+1</button>
-        <button onClick={restar}>-1</button>
-        <button onClick={reset}>reset</button>
-    </div>
-    )
+         <button onClick={sumar}>+1</button>
+         <button onClick={restar}>-1</button>
+         <button onClick={reset}>reset</button>
+     </div>
+     )
 
 
 
-render()
+ render()
